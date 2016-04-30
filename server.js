d8db541e3b5d90c506dade82789510274f66ebf1
mjrdnk <mjrdnk@gmail.com>
2016-04-30 12:06:57 +0200

    console.log students

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+var students;
+fs.readFile('./dsa.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  students = JSON.parse(data);
+  console.log(students);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
